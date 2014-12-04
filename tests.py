f91577edb1b9422bd1b2b47232095eb5f9b36830
jopape <paulojoao2005@yahoo.com.br>
2014-12-04 23:20:11 +0000

    arrumando testes

--- tests.py
+++ tests.py
@@ -4,5 +4,12 @@ class CalculatorTestCase(unittest.TestCase):
 
     def test_subtraction(self):
         ret = Calculator.subtraction(5,1)
-        self.assertEquals(5, ret)
+        self.assertEquals(4, ret)
+
+    def test_subtration1(self):
+        pass
+
+    def test_other_name(self):
+        ret = Calculator.subtraction(10,100)
+        self.assertEqual(-90, ret)
 
