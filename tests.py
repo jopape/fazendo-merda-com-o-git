f7949900a310f26d1c7c765517f9fd4bfd0670d2
jopape <paulojoao2005@yahoo.com.br>
2014-12-04 23:15:07 +0000

    adicionando metodo de soma

--- tests.py
+++ tests.py
@@ -6,3 +6,7 @@ class CalculatorTestCase(unittest.TestCase):
         ret = Calculator.subtraction(5,1)
         self.assertEquals(5, ret)
 
+    def test_sum(self):
+        ret = Calculator.sum(5,1)
+        self.assertEqual(6,ret)
+
